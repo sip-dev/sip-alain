852bbe5f4f13b34ee1d8856283d8cf96f46215ff
winpzs <winpzs@gmail.com>
2018-04-12 16:32:51 +0800

    remove ngx-countdown

--- src/delon/delon-shared.module.ts
+++ src/delon/delon-shared.module.ts
@@ -3,17 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 // delon
-import { NgZorroAntdExtraModule } from 'ng-zorro-antd-extra';
 import { AlainThemeModule } from '@delon/theme';
 import { AlainACLModule } from '@delon/acl';
 import { ZORROMODULES, ABCMODULES } from './delon.module';
 // i18n
 import { TranslateModule } from '@ngx-translate/core';
-
-// region: third libs
-import { CountdownModule } from 'ngx-countdown';
-const THIRDMODULES = [ CountdownModule ];
-// endregion
 
 // region: your componets & directives
 const COMPONENTS = [];
@@ -27,12 +21,9 @@ const DIRECTIVES = [];
         RouterModule,
         ReactiveFormsModule,
         ...ZORROMODULES,
-        NgZorroAntdExtraModule,
         AlainThemeModule.forChild(),
         ...ABCMODULES,
-        AlainACLModule,
-        // third libs
-        ...THIRDMODULES
+        AlainACLModule
     ],
     declarations: [
         // your components
@@ -45,13 +36,10 @@ const DIRECTIVES = [];
         ReactiveFormsModule,
         RouterModule,
         ...ZORROMODULES,
-        NgZorroAntdExtraModule,
         AlainThemeModule,
         ...ABCMODULES,
         // i18n
         TranslateModule,
-        // third libs
-        ...THIRDMODULES,
         // your components
         ...COMPONENTS,
         ...DIRECTIVES
