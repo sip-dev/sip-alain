8fd214bed0eccee4689983f362a4c1bb9213a07b
winpzs <winpzs@gmail.com>
2018-06-19 14:19:22 +0800

    OPT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,14 +31,14 @@ import { SharedModule } from './shared/shared.module';
       // **注：** 建议使用本地路径；以下为了减少 ng-alain 脚手架的包体大小引用了CDN，可能会有部分功能受影响
       js: [
         `//apps.bdimg.com/libs/ueditor/1.4.3.1/ueditor.config.js`,
-        `//apps.bdimg.com/libs/ueditor/1.4.3.1/ueditor.all.min.js`,
+        `//apps.bdimg.com/libs/ueditor/1.4.3.1/ueditor.all.min.js`
       ],
       options: {
-        UEDITOR_HOME_URL: `//apps.bdimg.com/libs/ueditor/1.4.3.1/`,
-      },
+        UEDITOR_HOME_URL: `//apps.bdimg.com/libs/ueditor/1.4.3.1/`
+      }
     }),
     NgxTinymceModule.forRoot({
-      baseURL: '//cdn.bootcss.com/tinymce/4.7.4/',
+      baseURL: '//cdn.bootcss.com/tinymce/4.7.4/'
     })
   ],
   providers: [
