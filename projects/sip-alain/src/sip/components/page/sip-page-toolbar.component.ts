b1a0264d723334fc3e305986d8c3e217698adfad
zhudc <zhudc@bingosoft.net>
2018-08-15 16:54:15 +0800

    UI提交！

--- projects/sip-alain/src/sip/components/page/sip-page-toolbar.component.ts
+++ projects/sip-alain/src/sip/components/page/sip-page-toolbar.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'sip-page-toolbar',
-    template: `<div class="mb-md"><ng-content></ng-content></div>`,
+    template: `<div class="sip-page-toolbar"><ng-content></ng-content></div>`,
     styles: []
 })
 export class SipPageToolbarComponent {
